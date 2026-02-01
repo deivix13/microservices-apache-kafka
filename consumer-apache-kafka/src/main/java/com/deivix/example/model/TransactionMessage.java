a9c0fa6b1ad05949d0b160d74e1e6e5b1b6af1b4
deivix13 <deividcode13@gmail.com>
2026-02-01 15:19:51 +0100

    chore: updated classes and pom

--- consumer-apache-kafka/src/main/java/com/deivix/example/model/TransactionMessage.java
+++ consumer-apache-kafka/src/main/java/com/deivix/example/model/TransactionMessage.java
@@ -1,24 +1,34 @@
 package com.deivix.example.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class TransactionMessage {
 
+    @NonNull
     private Long transactionID;
-    private Event event;
-    private Double amount;
-    private Status status;
 
-    public enum Status {
-        SUBMITED, STARTED, PENDING, FINISHED, TERMINATED
-    }
+    @NotBlank(message = "Event cannot be blank")
+    private Event event;
+
+    @Positive
+    private Double amount;
+
+    @NotBlank(message = "Status cannot be blank")
+    private Status status;
 
     public enum Event {
         WITHDRAW, DEPOSIT
     }
+    public enum Status {
+        SUBMITTED, STARTED, PENDING, FINISHED, TERMINATED
+    }
+
 }
